0b511dff9c0f6b7df87d55a8f85a93b08595906c
ShivaniChavan-tech <102436108+ShivaniChavan-tech@users.noreply.github.com>
2022-04-23 18:22:35 -0400

    Add files via upload

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Post = () => {
+return (
+	<footer className="App-footer">
+		<p>Copyright © Shivani's Blog</p>
+	</footer>
+);
+};
+
+export default Post;
---
+++ src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Post = () => {
+return (
+	<header className="App-header">
+        <h1>Welcome to my World!!!!</h1>
+        <div className="links">
+          {/* <Link to="/Blog"> Blog </Link> */}
+        </div>
+      </header>
+);
+};
+
+export default Post;
